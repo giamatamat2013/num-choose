91a57bc3a4f87abc86ae85f7e45839be71d1ac28
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-09 16:28:55 +0300

    Fix output message for negative numbers and enhance response for specific inputs

--- num/main.py
+++ num/main.py
@@ -1,12 +1,17 @@
+import time
+
 num = input ("Enter a number: ")
 
 if num == "42":
     print ("This is my favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
 elif num == "67":
     print ("This is a bad trend.")
+    print ("We gonna kick you out.")
+    time.sleep(5)
+    exit
 elif num >= "1000000":
     print ("This is a BIG number")
 elif num <= "0":
-    print ("This is minus")
+    print ("This is negative number")
 else:
     print ("This is YOUR favorite number")
