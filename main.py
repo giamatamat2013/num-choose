a4ed81ea3b2f72ed09f32c356225ca7662cc991a
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-09 16:06:05 +0300

    Enhance output messages for specific number inputs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 x = input ("Enter a number: ")
 
 if x == "42":
-    print ("This is my favorite number!")
+    print ("This is my favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
 elif x == "67":
     print ("This is a bad trend.")
+else:
+    print ("This is YOUR favorite number, I guess.")
