f7fbe113e11eb1865d894c4038fa425e90f574e1
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-09 16:10:16 +0300

    Add condition to handle large number inputs

--- num/main.py
+++ num/main.py
@@ -4,5 +4,7 @@ if x == "42":
     print ("This is my favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
 elif x == "67":
     print ("This is a bad trend.")
+elif x >= "1000000":
+    print ("This is a BIG number")
 else:
     print ("This is YOUR favorite number")
