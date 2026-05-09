727a15b5477e4c12a67459ba9ec75182979b54c3
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-09 16:10:43 +0300

    Refactor variable name from 'x' to 'num' for clarity

--- num/main.py
+++ num/main.py
@@ -1,10 +1,10 @@
-x = input ("Enter a number: ")
+num = input ("Enter a number: ")
 
-if x == "42":
+if num == "42":
     print ("This is my favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
-elif x == "67":
+elif num == "67":
     print ("This is a bad trend.")
-elif x >= "1000000":
+elif num >= "1000000":
     print ("This is a BIG number")
 else:
     print ("This is YOUR favorite number")
