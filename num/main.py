08bda94177a2a6c895b9ac7edbecf3377575965f
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-09 16:36:32 +0300

    Fix sleep duration and adjust conditions for number inputs

--- num/main.py
+++ num/main.py
@@ -6,11 +6,11 @@ if num == "42":
     print ("This is my favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
 elif num == "67":
     print ("This is a bannd trend.")
-    time.sleep(3)
+    time.sleep(1)
     exit
-elif num >= "1000000":
-    print ("This is a BIG number")
 elif num <= "0":
     print ("This is negative number")
+elif num == "23":
+    print ("This is amit favorite number")
 else:
     print ("This is YOUR favorite number")
