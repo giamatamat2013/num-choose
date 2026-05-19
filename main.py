3301236defb583e62227f6008313355843e5a982
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-05-19 15:34:27 +0300

    Fix input type comparisons in num_choose function for consistency

--- main.py
+++ main.py
@@ -18,48 +18,48 @@ def num_choose():
     
     num = input ("Enter a number: ")
     
-    if num == 42:
+    if num == "42":
         print ("This is Itamar's favorite number! And the answer to the Ultimate Question of Life, The Universe, and Everything.")
-    elif num == 67:
+    elif num == "67":
         print ("This is a bannd trend.")
         time.sleep(1)
         exit
-    elif num < 0:
+    elif num < "0":
         print ("This is negative number.")
-    elif num == 23:
+    elif num == "23":
         print ("This is amit's favorite number!")
-    elif num == 0:
+    elif num == "0":
         print ("This is Eitan's favorite number!")
-    elif num == 2013:
+    elif num == "2013":
         print ("What a creativity, did you copy from the title?")
-    elif num == 6:
+    elif num == "6":
         print ("This is the most skipped number!")
-    elif num == 12:
+    elif num == "12":
         print ("This is the basis of the counting of Tatarism.")
-    elif num == 7:
+    elif num == "7":
         print ("This is the most popular number in the world!")
-    elif num == 365:
+    elif num == "365":
         print ("This is the number of days in a year!")
-    elif num == 10:
+    elif num == "10":
         print ("This is Carmel's favorite number!")
-    elif num == 2019:
+    elif num == "2019":
         print ("This is Omer's birthday year!")
-    elif num == 3:
+    elif num == "3":
         print ("This is the closest full number to pi.")
-    elif num == 3.14: 
+    elif num == "3.14": 
         print ("This is two numbers after the dot of pi.")
-    elif num ==8:
+    elif num =="8":
         print ("It's a rotated infinity symbol.")
-    elif num == 123 or num == 1234 or num == 12345 or num == 123456 or num == 1234567 or num == 12345678 or num == 123456789 or num == 1234567890:
+    elif num == "123" or num == "1234" or num == "12345" or num == "123456" or num == "1234567" or num == "12345678" or num == "123456789" or num == "1234567890":
         print ("You really suck at choosing passwords!")
-    elif num == 404:
-        print ("page not found")
+    elif num == "404":
+        print ("Page not found")
         time.sleep(5)
-    elif num == 69 or num == 96:
+    elif num == "69" or num == "96":
         print ("This number stays the same even if you rotate it 180 degrees.")
-    elif num == 1:
+    elif num == "1":
         print ("This is the first natural number.")
-    elif num > 999999999999:
+    elif num > "999999999999":
         print ("This number is too large for me to understand, try a smaller one.")
     else:
         print ("This is YOUR favorite number!")
